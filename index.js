fc9b0214ba160e3bcacf181f5d3b452c380c5468
ovaldi <232280074@qq.com>
2016-03-10 17:24:15 +0800

    replace tab to spaces

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 ;(function (factory, global){
-  if(typeof define === 'function' && define.amd){
-    define(factory);
-  }
-  else if(typeof module === 'object' && typeof module.exports === 'object'){
-    module.exports = factory();
-  }
-  else{
-    global.curry = factory();
-  }
+    if(typeof define === 'function' && define.amd){
+      define(factory);
+    }
+    else if(typeof module === 'object' && typeof module.exports === 'object'){
+      module.exports = factory();
+    }
+    else{
+      global.curry = factory();
+    }
 })(function (){
-	return function curry(fn){
+    return function curry(fn){
 
-		if(typeof fn !== 'function'){
-			throw new Error('TypeError: expected a function');
-		}
-		
-		return function(){
-			var args = [];
+        if(typeof fn !== 'function'){
+            throw new Error('TypeError: expected a function');
+        }
+        
+        return function(){
+            var args = [];
 
-			function wrapper(){
-				args.push.apply(args, arguments);
+            function wrapper(){
+                args.push.apply(args, arguments);
 
-				if(fn.length === args.length){
-					return fn.apply(null, args);
-				}
+                if(fn.length === args.length){
+                    return fn.apply(null, args);
+                }
 
-				return wrapper;
-			}
+                return wrapper;
+            }
 
-			return wrapper.apply(null, arguments);
-		};
-	};
+            return wrapper.apply(null, arguments);
+        };
+    };
 }, this);
